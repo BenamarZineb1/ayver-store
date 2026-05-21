fd4858319929f9c1ec89db8ed6d3ef6354d39b2f
Zineb BENAMAR <zineb.benamar2@usmba.ac.ma>
2026-05-21 21:05:20 +0100

    fix: dynamic api route

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-
+export const dynamic = "force-dynamic";
 interface RouteParams {
   params: Promise<{ id: string }> | { id: string };
 }
